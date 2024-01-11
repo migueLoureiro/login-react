ffce051538613dea4370fced157ca3c1d22d0e37
jose.loureiro <jose.loureiro@olx.com>
2024-01-11 14:57:35 +0000

    Updates

---
+++ precache-manifest.e06e2265b7707abf6558474cd4c7b532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa06c25bdb9ca50a8d8a21d8647f401",
+    "url": "/migueLoureiro/login-react/index.html"
+  },
+  {
+    "revision": "b1044fa478e3d9896b74",
+    "url": "/migueLoureiro/login-react/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/migueLoureiro/login-react/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/migueLoureiro/login-react/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "b1044fa478e3d9896b74",
+    "url": "/migueLoureiro/login-react/static/js/main.704f432d.chunk.js"
+  },
+  {
+    "revision": "876bbc342707486475f2",
+    "url": "/migueLoureiro/login-react/static/js/runtime~main.4b4dd725.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/migueLoureiro/login-react/static/media/logo.06e73328.svg"
+  }
+]);
