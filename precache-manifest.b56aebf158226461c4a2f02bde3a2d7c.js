0d778967b10dccbb88f5536822ae33c972e1da18
jose.loureiro <jose.loureiro@olx.com>
2024-01-12 11:43:11 +0000

    Updates

---
+++ precache-manifest.b56aebf158226461c4a2f02bde3a2d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c410c2b8e415c5e08d78de66412cda",
+    "url": "/login-react/index.html"
+  },
+  {
+    "revision": "dd9ef27d502fa158643c",
+    "url": "/login-react/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/login-react/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/login-react/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "dd9ef27d502fa158643c",
+    "url": "/login-react/static/js/main.6b5e45a9.chunk.js"
+  },
+  {
+    "revision": "d55055e123cc8429f81e",
+    "url": "/login-react/static/js/runtime~main.242270f0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/login-react/static/media/logo.06e73328.svg"
+  }
+]);
